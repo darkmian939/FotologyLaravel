8da1986144481f0a0329b632d5f9be7362e56bc0
Nicolle <marqueznicolle421@gmail.com>
2024-06-10 12:30:34 -0500

    feat(FronEnd): actualizacion vista registro fotografo - 100% - se quito el campo de descripcion

--- resources/views/InicioDeSesion/registrofotografo.blade.php
+++ resources/views/InicioDeSesion/registrofotografo.blade.php
@@ -23,26 +23,27 @@
                     <i class='bx bxl-microsoft'></i>
                 </div>
             </div>
-            <h2 class="create-account">Crear una cuenta</h2>
+            <p class="create-account">Crear una cuenta gratis</p>
             <input type="text" name="nameUser" placeholder="Nombre completo" required>
             <input type="text" name="username" placeholder="Nombre de usuario" required>
             <input type="tel" name="phone" placeholder="Teléfono" required>
             <input type="email" name="email" placeholder="Correo electrónico" required>
             <input type="text" name="adress" placeholder="Dirección" required>
             <input type="date" name="birthday" placeholder="Fecha de nacimiento" required>
-            <textarea name="description" placeholder="Descripción"></textarea>
             <input type="password" name="contrasena" placeholder="Contraseña" required>            
             <input type="file" name="foto_perfil" accept="image/*" required>
             <input type="submit" value="Registrarse" name="registrarse">   
         </form>
 
         <div class="welcome-back">
-            <div class="message">
-                <h2>Bienvenido a Fotology</h2>
-                <p>Si ya tienes una cuenta, por favor inicia sesión aquí</p>
-                <button><a href="{{ route('InicioDeSesion.usuario') }}" class="sign-up-btn">Iniciar Sesión</a></button>
-            </div>
-        </div>
+    <div class="message">
+        <img src="Recursos/LOGOA.png" alt="Bienvenido a Fotology" class="welcome-image">
+        <h2>Bienvenido a Fotology</h2>
+        <p>Si ya tienes una cuenta, por favor inicia sesión aquí</p>
+        <button class="custom-button">
+            <a href="{{ route('InicioDeSesion.fotografo') }}" class="sign-up-btn">Iniciar Sesión</a>
+        </button>
+    </div>
     </div>
     <script src="java.js"></script>
 </body>
